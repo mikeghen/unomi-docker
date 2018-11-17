ce3af4f2f6560f5862298f3982443c2c6014cd18
Mike Ghen <mike.ghen@gmail.com>
2018-11-17 11:31:54 -0500

    Added longer delay for unomi startup, modify ES

--- start_with_ip.sh
+++ start_with_ip.sh
@@ -2,7 +2,7 @@
 
 sed -i "s/elasticSearchAddresses=localhost:9300/elasticSearchAddresses=${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/g" /opt/apache-unomi/etc/org.apache.unomi.persistence.elasticsearch.cfg
 $KARAF_HOME/bin/start
-sleep 60
+sleep 120
 $KARAF_HOME/bin/client "unomi:start"
 echo "Started Unomi"
 tail -f $KARAF_HOME/data/log/karaf.log
